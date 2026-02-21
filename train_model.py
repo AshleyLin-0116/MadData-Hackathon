9ea340630b901c4c7c3a1cdec816c710ae8a01ad
Ashley Lin <alin77@wisc.edu>
2026-02-21 15:05:52 -0600

    Updated stress model

--- train_model.py
+++ train_model.py
@@ -4,7 +4,7 @@ import os
 
 class Avatar_Model:
     stress = None
-    mental_health = pd.read_csv("C:\Users\Ashle\Downloads\Student_Mental_Health_Survey", compression = "zip")
-    student_stress = pd.read_csv("C:\Users\Ashle\Downloads\Student_Stress_Analysis", compression = "zip")
-    sleep_health = pd.read_csv("C:\Users\Ashle\Downloads\Sleep_Health_and_Lifestyle_Dataset", compression = "zip")
+    mental_health = pd.read_csv("MentalHealthSurvey.csv")
+    sleep_health = pd.read_csv("expanded_sleep_health_dataset.csv")
+    student_stress = pd.read_csv("Student Stress Factors.csv")
     
